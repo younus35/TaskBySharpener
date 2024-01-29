def36c724a39c3c007a2aa77f727447ee7bdee61
younus35 <154969855+younus35@users.noreply.github.com>
2024-01-29 23:32:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+<p>I want you to create a new function called updateLastUserActivityTime which returns a promise, we will use this function to update the user's last activity time, and when it will resolve, it should send back the updated last activity time for the user (it should take 1 second to execute).
+Every time the user creates a post, updateLastUserActivityTime should be called so that the user's last activity time gets updated.
+When both the promises resolve (createPost and updateLastUserActivityTime ),
+I want you to console log all the posts and lastActivityTime of the user. [If stuck for long watch the hint 2]
+Once both the above promises are resolved, I want you to delete the last post by calling the deletePost promise. Once successfully deleted, I want you to log the remaining Posts.<p>
+
+
 const posts = ["start"] //here const is refering to the reference of an array so we can push elements to the array 
 const users = {name:"younus",activityTime:new Date().toISOString()};
 function updateLastUserActivityTime(){
